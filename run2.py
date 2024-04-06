9aaf269b307574797c785f4514f1efca8f142f27
Siang <tasd951@gmail.com>
2024-04-06 22:59:09 +0800

    finish 音量調整器

--- run2.py
+++ run2.py
@@ -3,5 +3,5 @@ import os
 
 # subprocess.run(['start','cmd','/k','.\\audio_testenv\\Scripts\\activate','python','-m','controller'],shell=True)
 
-os.system('start cmd.exe cmd /k "audio_testenv\\Scripts\\activate"')
-os.system('cmd /k "python -m controller"')
+os.system('start cmd.exe cmd /k "cd .. && audio_testenv\\Scripts\\activate && python -m controller"')
+# os.system('cmd /k "python -m controller"')
